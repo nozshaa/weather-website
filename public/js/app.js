77c0a16bbd13dd6a5be474843b01fab6cf2b0156
Nastaran Moghadasi <nastaran.moghadasi@gmail.com>
2021-12-20 22:28:13 +0100

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,28 @@
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+const result = document.querySelector(".result");
+const messageOne = document.querySelector("#message-1");
+const messageTwo = document.querySelector("#message-2");
+
+weatherForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const location = search.value;
+  messageOne.textContent = "Loading...";
+  messageTwo.textContent = "";
+
+  fetch(`http://localhost:3000/weather?address=${location}`)
+    .then((res) => res.json())
+    .then((data) => {
+      //   result.innerHTML = "k";
+      if (data.error) {
+        messageOne.textContent = data.error;
+        console.log(data.error);
+      } else {
+        messageOne.textContent = data.location;
+        messageTwo.textContent = data.forecastData;
+      }
+    });
+
+  console.log("testing!: ", result);
+});
---
+++ src/utils/forecast.js
@@ -0,0 +1,20 @@
+const request = require("request");
+
+const forecast = (lat, long, callback) => {
+  const url = `http://api.weatherstack.com/current?access_key=e588495757cb7178ff47e074590bce1c&query=${lat},${long}`;
+
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Unable to find a network!");
+    } else if (body.error) {
+      callback("Unable to find loaction");
+    } else {
+      callback(
+        undefined,
+        `The temperiture is ${body.current.temperature} and it feels like ${body.current.feelslike}`
+      );
+    }
+  });
+};
+
+module.exports = forecast;
